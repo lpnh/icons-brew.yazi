74f02aa47587e6191cae01db295ddf2ac8f400d0
lpnh <paniguel.lpnh@gmail.com>
2025-08-05 10:42:14 -0300

    implement explicit color mapping

--- brew.lua
+++ brew.lua
@@ -1,7 +1,6 @@
 local config = require("config")
 local dark = require("nvim-web-devicons.icons-default")
 local light = require("nvim-web-devicons.icons-light")
-local prepare = require("prepare")
 
 function rearrange(by)
 	local map = {}
@@ -9,10 +8,10 @@ function rearrange(by)
 	for k, v in pairs(dark[source]) do
 		map[k] = map[k] or {}
 		map[k].icon = v.icon
-		map[k].fg_dark = v.color:lower()
+		map[k].number_dark = tonumber(v.cterm_color)
 	end
 	for k, v in pairs(light[source]) do
-		map[k].fg_light = v.color:lower()
+		map[k].number_light = tonumber(v.cterm_color)
 	end
 	return map
 end
@@ -20,14 +19,14 @@ end
 function fill(map)
 	local list = {}
 	for k, v in pairs(map) do
-		list[#list + 1] = { name = k, text = v.icon, fg_dark = v.fg_dark, fg_light = v.fg_light }
+		list[#list + 1] = { name = k, text = v.icon, number_dark = v.number_dark, number_light = v.number_light }
 	end
 	table.sort(list, function(a, b) return a.name:lower() < b.name:lower() end)
 	local dark_blend, light_blend = "", ""
 	local dark, light = "", ""
 	for _, v in ipairs(list) do
-		dark_blend = prepare.matching_colors(v.fg_dark, config.dark_colors_table, config.factors, config.precise_search)
-		light_blend = prepare.matching_colors(v.fg_light, config.light_colors_table, config.factors, config.precise_search)
+		dark_blend = config.get_color(config.dark, v.number_dark, v.name)
+		light_blend = config.get_color(config.light, v.number_light, v.name)
 
 		dark = dark .. string.format('\t{ name = "%s", text = "%s", fg = "%s" },\n', v.name, v.text, dark_blend)
 		light = light .. string.format('\t{ name = "%s", text = "%s", fg = "%s" },\n', v.name, v.text, light_blend)
@@ -35,9 +34,9 @@ function fill(map)
 	return dark, light
 end
 
-function save(typ, files, exts)
+function save(th, files, exts)
 	local t = "empty-theme.toml"
-	local p = string.format("theme-%s.toml", typ)
+	local p = string.format("theme-%s.toml", th)
 	local s = io.open(t, "r"):read("*a")
 	s = s:gsub("files = %[%]", string.format("files = [\n%s]", files))
 	s = s:gsub("exts = %[%]", string.format("exts = [\n%s]", exts))
